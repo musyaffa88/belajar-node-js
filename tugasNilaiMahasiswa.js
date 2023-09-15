7dd4a962788822af4e33d1b31df9ebc831ebcd75
musyaffa88 <farismusyaffa88@gmail.com>
2023-09-15 19:12:51 +0700

    tugas nilai mahasiswa

--- tugasNilaiMahasiswa.js
+++ tugasNilaiMahasiswa.js
@@ -1,33 +1,50 @@
 const prompt = require('prompt-sync')();
 
-const inputNilai = prompt('Nilai Mahasiswa : ');
-const nilai = Number(inputNilai);
-
-function nilaiMahasiswa() {
-    if (nilai < 0 || nilai > 100){
-        console.log('Nilai Tidak Boleh Kurang dari 0 atau Lebih dari 100');
-        console.log('======================================');
-    } else{
-        if (nilai >= 80 && nilai <= 100) {
-          console.log('Mahasiswa mendapatlan grade : A');
-          console.log('======================================');
-          } else if (nilai >= 60 && nilai < 80) {
-            console.log('Mahasiswa mendapatlan grade : B');
+let input_again = true;
+let nilai = prompt('Nilai Mahasiswa : ');
+while (input_again) {
+      if (nilai < 0 || nilai > 100 || nilai == "" ){
+            console.log('Nilai Tidak Boleh Kurang dari 0 atau Lebih dari 100 dan Tidak Boleh Kosong');
             console.log('======================================');
-          } else if (nilai >= 40 && nilai < 60) {
-            console.log('Mahasiswa mendapatlan grade : C');
-            console.log('======================================');
-          } else if (nilai >= 20 && nilai < 40) {
-            console.log('Mahasiswa mendapatlan grade : D');
-            console.log('======================================');
-          } else if (nilai > 0 && nilai < 20){
-            console.log('Mahasiswa mendapatlan grade : E');
-            console.log('======================================');
-          } else if(nilai == 0){
-            console.log('Mahasiswa mendapatlan grade : F');
-            console.log('======================================');
+            input_again = false;
+            if (input_again == false) {
+              console.log('Masukkan Ulang Nilai Mahasiswa');
+              nilai = prompt('Nilai Mahasiswa : ');
+              input_again = true;
+            }
+        } else{
+            if (nilai >= 80 && nilai <= 100) {
+              console.log('Mahasiswa mendapatkan grade : A');
+              console.log('======================================');
+              input_again = false;
+              } else if (nilai >= 60 && nilai < 80) {
+                console.log('Mahasiswa mendapatkan grade : B');
+                console.log('======================================');
+                input_again = false;
+              } else if (nilai >= 40 && nilai < 60) {
+                console.log('Mahasiswa mendapatkan grade : C');
+                console.log('======================================');
+                input_again = false;
+              } else if (nilai >= 20 && nilai < 40) {
+                console.log('Mahasiswa mendapatkan grade : D');
+                console.log('======================================');
+                input_again = false;
+              } else if (nilai > 0 && nilai < 20){
+                console.log('Mahasiswa mendapatkan grade : E');
+                console.log('======================================');
+                input_again = false;
+              } else if(nilai == 0){
+                console.log('Mahasiswa mendapatkan grade : F');
+                console.log('======================================');
+                input_again = false;
+              } else {
+                console.log("Anda tidak memasukkan nilai mahasiswa dengan benar");
+                input_again = false;  
+                if (input_again == false) {
+                  console.log('Masukkan Ulang Nilai Mahasiswa');
+                  nilai = prompt('Nilai Mahasiswa : ');
+                  input_again = true;
+                }
+              }
           }
-      }
 }
-
-nilaiMahasiswa(nilai)
